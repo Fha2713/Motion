df124e257afd37f7b01dbe402023b962ae976f39
Franz Haberfellner <fha2713@students.fhv.at>
2025-01-21 20:40:20 +0100

    fullscreen maybe fixed

--- js/armaster.js
+++ js/armaster.js
@@ -50,17 +50,18 @@ document.querySelector('#goat-marker').addEventListener('click', () => {
 
 
 document.getElementById('fullScreenButton').addEventListener('click', () => {
-  const scene = document.getElementById('view');
+  const view = document.getElementById('view'); // The <body> element with id="view"
 
   if (!document.fullscreenElement) {
-    // Enter fullscreen
-    scene.requestFullscreen()
-        .then(() => console.log('Fullscreen mode activated'))
-        .catch(err => console.error(`Failed to enable fullscreen: ${err.message}`));
+    // Request fullscreen mode
+    view.requestFullscreen()
+        .then(() => console.log('Entered fullscreen mode'))
+        .catch(err => console.error(`Error entering fullscreen: ${err}`));
   } else {
-    // Exit fullscreen
+    // Exit fullscreen mode
     document.exitFullscreen()
-        .then(() => console.log('Fullscreen mode deactivated'))
-        .catch(err => console.error(`Failed to exit fullscreen: ${err.message}`));
+        .then(() => console.log('Exited fullscreen mode'))
+        .catch(err => console.error(`Error exiting fullscreen: ${err}`));
   }
 });
+
